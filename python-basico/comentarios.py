fd89e1bf901ae519a43172df36434f6769c8d29a
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-14 23:52:38 -0300

    Aula sobre comentários

---
+++ python-basico/comentarios.py
@@ -0,0 +1,9 @@
+# isso e um comentario
+print('Ola mundo!')
+# Este e o inicio de uma caminhada
+print('Inicio de uma jornada')  #Bora la :D
+print('#partiu programar em python')
+
+""" O python censurou os acentos,
+mas não entendi o porquê, pois estou utilizando
+ascentos dentro dos comentários"""
