2a1ce9458d89aafa94d793af8cfc2c10c6d5a77e
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-14 23:55:29 -0300

    Aula sobre comentários

--- python-basico/comentarios.py
+++ python-basico/comentarios.py
@@ -1,9 +1,15 @@
-# isso e um comentario
-print('Ola mundo!')
+# -*- coding: utf-8 -*-
+# isso é um comentário
+print('Olá mundo!')
 # Este e o inicio de uma caminhada
-print('Inicio de uma jornada')  #Bora la :D
+print('Início de uma jornada')  #Bora lá :D
 print('#partiu programar em python')
 
 """ O python censurou os acentos,
 mas não entendi o porquê, pois estou utilizando
 ascentos dentro dos comentários"""
+
+#descobri pesquisando na internet que é só
+#utilizar("# -*- coding: utf-8 -*-")
+
+"""Show de bola"""
